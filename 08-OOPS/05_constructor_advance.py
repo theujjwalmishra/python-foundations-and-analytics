a00cf57d21470de771b8aabae4670191a32d2c29
Ujjwal Mishra <ujjwalmishra.cse2024@agra.sharda.ac.in>
2026-09-09 11:34:30 +0530

    add

---
+++ 08-OOPS/05_constructor_advance.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self , name ,salary,language ):
+        self.name = name
+        self.salary = salary
+        self.language = language
+ujjwal = Employee(
+    "ujjwal","1.5 Trillion","Python"
+)
+
+print(ujjwal.name , ujjwal.salary , ujjwal.language)
